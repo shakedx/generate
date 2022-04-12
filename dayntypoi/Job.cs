fef6282c657bec02f6e9401566143072f54e0552
shakedx <kolesnikov_danil123@mail.ru>
2022-04-12 02:45:20 +0200

    Добавьте файлы проекта.

---
+++ dayntypoi/Job.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayntypoi
+{
+    public class Job
+    {
+        private int n;
+        private Prog_Properties[] cand;
+        private string[] strCand;
+        private Prog_Properties pattern;
+        private Prog_Properties currentScale;
+        private Random random;
+
+        public Job()
+        {
+            n = 15;
+            cand = new Prog_Properties[n];
+            strCand = new string[n];
+            random = new Random();
+        }
+
+        public Job(int n)
+        {
+            this.n = n;
+            cand = new Prog_Properties[n];
+            strCand = new string[n];
+            random = new Random();
+        }
+
+        public Job(Prog_Properties[] pp)
+        {
+            this.n = pp.Length;
+            cand = new Prog_Properties[n];
+            strCand = new string[n];
+            random = new Random();
+        }
+        public Prog_Properties Pattern
+        {
+            set { pattern = value; }
+        }
+
+        public void FormCanda()
+        {
+            int properties = 5;
+            int p = 0, q = 0, currentProps = 0;
+            string strQ;
+            for (int i = 0; i < n; i++)
+            {
+                currentProps = 0; strQ = "";
+                for (int j = 0; j < properties; j++)
+                {
+                    p = random.Next(2);
+                    q = (int)Math.Pow(2, j);
+                    if (p == 1)
+                    {
+                        currentProps += q;
+                        strQ += (Prog_Properties)q + ", ";
+                    }
+                }
+                cand[i] = (Prog_Properties)currentProps;
+                if (strQ != "") strCand[i] = strQ.Remove(strQ.Length - 2);
+                else strCand[i] = "";
+            }
+        }
+        public string[] GetStrCands()
+        {
+            return strCand;
+        }
+        public Prog_Properties[] GetCands()
+        {
+            return cand;
+        }
+        //список кандидатов, которые обладают свойствами заданными
+        //свойствами
+        public string[] CandsHavePet()
+        {
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if ((cand[i] & pattern) == pattern)
+                {
+                    k++;
+                }
+            }
+            string[] str = new string[k];
+            k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if ((cand[i] & pattern) == pattern)
+                {
+                    str[k] = "cand[" + (i + 1) + "]";
+                    k++;
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ dayntypoi/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dayntypoi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public enum Prog_Properties
+    {
+        Russian = 1, Math = 2, Physics = 4, English = 6, Informatika = 8, Literature = 10, Germany = 12
+    }
+    public partial class MainWindow : Window
+    {
+        private Job job;
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (string str in Enum.GetNames(typeof(Prog_Properties)))
+            {
+                CheckBox checkBox = new CheckBox
+                {
+                    Content = str,
+                    MaxHeight = 30,
+                    IsChecked = false,
+                    Margin = new Thickness(10)
+                };
+                checkBox.Checked += checkBox_checked;
+                Patterns.Children.Add(checkBox);
+            }
+        }
+
+        private void checkBox_checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            Prog_Properties properties = (Prog_Properties)Enum.Parse(typeof(Prog_Properties),
+                                        checkBox.Content.ToString());
+            job.Pattern = properties;
+            Result.Text = "";
+            foreach (string str in job.CandsHavePet())
+            {
+                Result.Text += str + "\n";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Text = "";
+            int n = 15;
+            job = new Job(n);
+            job.FormCanda();
+            Prog_Properties[] cand = job.GetCands();
+            for (int i = 0; i < Patterns.Children.Count; i++)
+            {
+                (Patterns.Children[i] as CheckBox).IsChecked = false;
+            }
+            string[] strCand = job.GetStrCands();
+            for (int i = 0; i < n; i++)
+            {
+                Result.Text += "Свойства кандидата " + (i + 1) + "-" + cand[i] + "\n";
+                Result.Text += strCand[i] + '\n';
+            }
+        }
+    }
+}
